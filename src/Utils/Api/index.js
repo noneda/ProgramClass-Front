1fd0ace0f57f790c0f9ceeed1364838aeaa82f87
noneda <nonejda@gmail.com>
2024-10-27 11:19:12 -0500

    0.0.9 / ... :#

--- src/Utils/Api/index.js
+++ src/Utils/Api/index.js
@@ -21,3 +21,29 @@ export const allClass = async () => {
     console.error(err);
   }
 };
+
+export const UniqueClass = async (id) => {
+  try {
+    const res = await axios.get(`http://127.0.0.1:8000/Api/DataClass/${id}`);
+    if (res.status === 200) {
+      const { Class, Sections } = res.data;
+      BubbleSort(Sections, "Order");
+      console.log(Sections);
+    }
+  } catch (err) {}
+};
+
+const BubbleSort = (a, par) => {
+  var swapped;
+  do {
+    swapped = false;
+    for (var i = 0; i < a.length - 1; i++) {
+      if (a[i][par] > a[i + 1][par]) {
+        var temp = a[i];
+        a[i] = a[i + 1];
+        a[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+};
